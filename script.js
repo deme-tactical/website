7088dc108b382a5afec67f8ba724a26b2d3487cb
deme-tactical <deme.tactical@protonmail.com>
2026-06-15 13:04:04 +0200

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-
-document.querySelectorAll('nav a').forEach(a=>{
- a.addEventListener('click',e=>{
-  e.preventDefault();
-  document.querySelector(a.getAttribute('href')).scrollIntoView({behavior:'smooth'});
- });
-});
